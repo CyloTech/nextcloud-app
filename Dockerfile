e1d79855ae5e266f5b9c374213fd663e6a1d2cd4
Mylo <gavin@cylo.io>
2018-06-15 17:11:46 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM repo.cylo.io/ubuntu-lemp
+
+# Disable Supervisor on the parent image, this allows us to run commands after the parent has finished installing.
+ENV START_SUPERVISOR=false
+
+# Declare Environment variables required by the parent:
+ENV MYSQL_ROOT_PASS=mysqlr00t
+ENV DB_NAME=nextlcloud
+
+# Nextcloud Environment variables
+ENV NEXTCLOUD_VER="13.0.4"
+ENV ADMIN_USER=admin
+ENV ADMIN_PASS=Letmein123
+
+RUN apt update; \
+    apt install -y unzip \
+                   samba \
+                   samba-dev \
+                   php-zip \
+                   php-gd \
+                   php-dom \
+                   php-mbstring \
+                   php-smbclient
+
+ADD /scripts /scripts
+RUN chmod -R +x /scripts
+
+ADD /sources /tmp
+
+ENTRYPOINT ["/scripts/Entrypoint.sh"]
---
+++ scripts/Entrypoint.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -x
+
+if [ ! -f /etc/nc_installed ]; then
+    # Install & Configure LEMP Stack
+    /bin/sh /scripts/lemp.sh
+
+    # Add our own NGINX Config.
+    rm -fr /home/appbox/config/nginx/sites-enabled/default-site.conf
+    mv /tmp/nextcloud.conf /home/appbox/config/nginx/sites-enabled/nextcloud.conf
+
+    # Configure PHP for Nextcloud
+echo "opcache.enable=1
+opcache.enable_cli=1
+opcache.interned_strings_buffer=8
+opcache.max_accelerated_files=10000
+opcache.memory_consumption=128
+opcache.save_comments=1
+opcache.revalidate_freq=1 " >> /etc/php/7.2/fpm/php.ini
+
+echo "env[HOSTNAME] = $HOSTNAME
+env[PATH] = /usr/local/bin:/usr/bin:/bin
+env[TMP] = /tmp
+env[TMPDIR] = /tmp
+env[TEMP] = /tmp" >> /home/appbox/config/php-fpm/pool.d/www.conf
+
+    mkdir -p /storage
+    # download app
+    cd /home/appbox/public_html
+    curl -o nextcloud.zip -L https://download.nextcloud.com/server/releases/nextcloud-"${NEXTCLOUD_VER}".zip
+    unzip nextcloud.zip
+    mv nextcloud/* .
+    mv nextcloud/.htaccess .
+    mv nextcloud/.user.ini .
+    rm -f nextcloud.zip
+    rm -fr nextcloud/
+
+    if [ -f /storage/admin/files/nextcloud.sql ]; then
+        echo "MySQL Backup Detected.. Restoring.."
+        /usr/bin/mysqld_safe &
+        sleep 10
+        mysql -uroot -p${MYSQL_ROOT_PASSWORD} ${DB_NAME} < /storage/admin/files/nextcloud.sql
+        rm -f /storage/admin/files/nextcloud.sql
+        pkill -9 mysql
+
+        mv /storage/admin/files/config.php /home/appbox/public_html/config/config.php
+
+echo "
+\$CONFIG['dbuser'] = 'root';
+\$CONFIG['dbpassword'] = 'mysqlr00t';
+" >> /home/appbox/public_html/config/config.php
+
+    else
+echo "<?php
+\$AUTOCONFIG = array(
+  'dbtype'        => 'mysql',
+  'dbname'        => 'nextcloud',
+  'dbuser'        => 'root',
+  'dbpass'        => '${MYSQL_ROOT_PASSWORD}',
+  'dbhost'        => 'localhost:3306',
+  'dbtableprefix' => 'nc_',
+  'adminlogin'    => '${ADMIN_USER}',
+  'adminpass'     => '${ADMIN_PASS}',
+  'directory'     => '/storage',
+);" > /home/appbox/public_html/config/autoconfig.php
+    fi
+
+    if [ ! -f /storage/.ocdata ]; then
+        touch /storage/.ocdata
+    fi
+
+    chown -R appbox:appbox /home/appbox/public_html
+
+    # set cronjob
+    crontab /tmp/crontab
+
+    chmod -R 0770 /storage
+    chown -R appbox:appbox /storage
+
+    # Finish Installation
+    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
+    touch /etc/nc_installed
+fi
+
+exec /usr/bin/supervisord -n -c /home/appbox/config/supervisor/supervisord.conf
